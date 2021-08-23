f69aeac11448dc3c8ed19e81c09c79001faa1581
Alexey Rudoy <ao.rudoy@yandex.ru>
2021-08-23 18:10:10 +0300

    fix: Project 15: ver 15.1.1 (fix App)

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -27,7 +27,6 @@ function App() {
   const [selectedCard, setSelectedCard] = React.useState({});
   const [currentUser, setCurrentUser] = React.useState({});
   const [cards, setCards] = React.useState([]);
-  console.log(cards)
 
   React.useEffect(() => {
     Promise.all([api.getUserProfile(), api.getInitialCards()])
